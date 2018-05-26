f5dec62b316544c69425d3537ae94ff9b394b7be
Duncan Taylor <duncan.taylor@sa.gov.au>
2018-05-26 17:24:17 +0930

    forgot to re-up the iterations after testing

--- run_Gnano_model.R
+++ run_Gnano_model.R
@@ -14,14 +14,14 @@ bugsFile = here(bugs_Model_R_Filename)
 sim = jags.model(file = bugsFile, data = bugsData, n.chains = 4)
 
 ## do a bit of burn in - no idea what is sufficient at this point
-#system.time(update(sim, 100000))
-system.time(update(sim, 100))
+system.time(update(sim, 100000))
+#system.time(update(sim, 100))
 
 ## The parameters are we interested
 parameters = c("pred", "lambda", "mu.dye", "mu.amp", "sigma.sq.dye", "sigma.sq.amp", "T", "loglik")
 ## run the model
-#sim.sample = coda.samples(model = sim, variable.names = parameters, n.iter = 50000, thin = 50)
-sim.sample = coda.samples(model = sim, variable.names = parameters, n.iter = 500, thin = 5)
+sim.sample = coda.samples(model = sim, variable.names = parameters, n.iter = 50000, thin = 50)
+#sim.sample = coda.samples(model = sim, variable.names = parameters, n.iter = 500, thin = 5)
 #creates graphs
 source("gNano.graphs.R")
 #saveDir <- here("graphs - uniform//")
